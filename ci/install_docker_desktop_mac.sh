c2f968caac0090361714c2e4374cce717e25df97
Reo Ono <1992reo@gmail.com>
2022-04-01 23:38:52 +0900

    remove unnecessary job

--- ci/install_docker_desktop_mac.sh
+++ ci/install_docker_desktop_mac.sh
@@ -6,9 +6,3 @@
 
 brew install --cask docker
 sudo /Applications/Docker.app/Contents/MacOS/Docker --unattended --install-privileged-components
-
-sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
-
-sleep 5
